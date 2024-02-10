921a61e738cde382ecbe7c9ded971c0fba5f5fd6
Kristian <10221425@ucn.dk>
2024-02-10 09:05:59 +0100

    Create & passing date to pages

---
+++ resources/js/Pages/Index/Index.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>Index</div>
+    <Link href="/hello">Show Page</Link>
+
+    The message is: {{ msg }}
+</template>
+
+<script setup>
+defineProps(['msg'])
+</script>
+
+<style lang="scss" scoped></style>
