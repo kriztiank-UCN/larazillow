a6a15f84f4c8e9b2112e7fdb2857031ceaaa9358
Kristian <10221425@ucn.dk>
2024-02-14 04:45:29 +0100

    header styling with Tailwind CSS

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,12 +3,12 @@
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
-    <title>Larazillow</title>
+    <title>LaraAds</title>
     @routes
     @vite('resources/js/app.js')
     @inertiaHead
   </head>
-  <body class="bg-white dark:bg-gray-900">
+  <body class="bg-white dark:bg-gray-900 text-gray-800 dark:text-gray-300">
     @inertia
   </body>
 </html>
