3becf3f7dcd42c858691dd17d5d2132f9a30d85f
Kristian <10221425@ucn.dk>
2024-02-16 13:09:44 +0100

    Login done

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,17 +3,33 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
 
 class AuthController extends Controller
 {
-    // Show Login Form
+    // Show form to create new session
     public function create()
     {
         return inertia('Auth/Login');
     }
-
-    public function store()
+    // Store new session (sign in user)
+    public function store(Request $request)
     {
+        // attempt returns true if authentication is successful or false if it fails
+        if (!Auth::attempt($request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string'
+            // true - remember me until logout
+        ]), true)) {
+            throw ValidationException::withMessages([
+                'email' => 'Authentication failed'
+            ]);
+        }
+
+        $request->session()->regenerate();
+
+        return redirect()->intended();
     }
 
     public function destroy()
