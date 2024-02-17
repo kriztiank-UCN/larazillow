35b8d5f8e1cc740a5332dd9cbda956dde214a8de
Kristian <10221425@ucn.dk>
2024-02-17 09:34:18 +0100

    User Registration Form

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -26,7 +26,8 @@
           </div>
         </div>
         <!-- show this if not logged in -->
-        <div v-else>
+        <div v-else class="flex items-center gap-2">
+          <Link :href="route('user-account.create')">Register</Link>
           <Link :href="route('login')">Sign-In</Link>
         </div>
       </nav>
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\IndexController;
 use App\Http\Controllers\ListingController;
+use App\Http\Controllers\UserAccountController;
 
 // create a new route, a new controller method and a new view for each resource
 
@@ -28,7 +29,9 @@ Route::resource('listing', ListingController::class);
 //   ->except(['create', 'store', 'edit', 'update', 'destroy']);
 
 
-
 Route::get('login', [AuthController::class, 'create'])->name('login');
 Route::post('login', [AuthController::class, 'store'])->name('login.store');
 Route::delete('logout', [AuthController::class, 'destroy'])->name('logout');
+
+Route::resource('user-account', UserAccountController::class)
+  ->only(['create']);
