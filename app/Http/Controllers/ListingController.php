b7be580e8d3c7a43b7f109c2ed434cb41320f680
Kristian <10221425@ucn.dk>
2024-02-18 22:10:55 +0100

    Filtering Using Local Query Scope

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -28,46 +28,62 @@ class ListingController extends Controller
      * Display a listing of the resource.
      */
     public function index(Request $request)
-    {
+    {   // -- #1 filters in the controller
+        // $filters = $request->only([
+        //     'priceFrom', 'priceTo', 'beds', 'baths', 'areaFrom', 'areaTo'
+        // ]);
+        // $query = Listing::orderByDesc('created_at');
+
+        // if ($filters['priceFrom'] ?? false) {
+        //     $query->where('price', '>=', $filters['priceFrom']);
+        // }
+
+        // if ($filters['priceTo'] ?? false) {
+        //     $query->where('price', '<=', $filters['priceTo']);
+        // }
+
+        // if ($filters['beds'] ?? false) {
+        //     $query->where('beds', $filters['beds']);
+        // }
+
+        // if ($filters['baths'] ?? false) {
+        //     $query->where('baths', $filters['baths']);
+        // }
+
+        // if ($filters['areaFrom'] ?? false) {
+        //     $query->where('area', '>=', $filters['areaFrom']);
+        // }
+
+        // if ($filters['areaTo'] ?? false) {
+        //     $query->where('area', '<=', $filters['areaTo']);
+        // }
+
+        // return inertia(
+        //     'Listing/Index',
+        //     [
+        //         'filters' => $filters,
+        //         'listings' => $query
+        //             ->paginate(10)
+        //             ->withQueryString()
+        //     ]
+        // );
+
+        // -- #2 scopeFilter in the model
         $filters = $request->only([
             'priceFrom', 'priceTo', 'beds', 'baths', 'areaFrom', 'areaTo'
         ]);
-        $query = Listing::orderByDesc('created_at');
-
-        if ($filters['priceFrom'] ?? false) {
-            $query->where('price', '>=', $filters['priceFrom']);
-        }
-
-        if ($filters['priceTo'] ?? false) {
-            $query->where('price', '<=', $filters['priceTo']);
-        }
-
-        if ($filters['beds'] ?? false) {
-            $query->where('beds', $filters['beds']);
-        }
-
-        if ($filters['baths'] ?? false) {
-            $query->where('baths', $filters['baths']);
-        }
-
-        if ($filters['areaFrom'] ?? false) {
-            $query->where('area', '>=', $filters['areaFrom']);
-        }
-
-        if ($filters['areaTo'] ?? false) {
-            $query->where('area', '<=', $filters['areaTo']);
-        }
-
 
         return inertia(
             'Listing/Index',
             [
                 'filters' => $filters,
-                'listings' => $query
-                    ->paginate(10)
-                    ->withQueryString()
+                'listings' => Listing::mostRecent()
+                ->filter($filters)
+                ->paginate(10)
+                ->withQueryString()
             ]
         );
+
     }
 
     /**
--- resources/js/Pages/Listing/Index.vue
+++ resources/js/Pages/Listing/Index.vue
@@ -1,11 +1,7 @@
 <template>
   <Filters :filters="filters" />
   <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
-    <Listing
-      v-for="listing in listings.data"
-      :key="listing.id"
-      :listing="listing"
-    />
+    <Listing v-for="listing in listings.data" :key="listing.id" :listing="listing" />
   </div>
 
   <div v-if="listings.data.length" class="w-full flex justify-center mt-4 mb-4">
@@ -14,11 +10,11 @@
 </template>
 
 <script setup>
+import Listing from '@/Pages/Listing/Index/Components/Listing.vue'
+import Pagination from '@/Components/UI/Pagination.vue'
 import Filters from '@/Pages/Listing/Index/Components/Filters.vue'
-import Listing from "@/Pages/Listing/Index/Components/Listing.vue";
-import Pagination from "@/Components/UI/Pagination.vue";
 defineProps({
   listings: Object,
   filters: Object,
-});
+})
 </script>
