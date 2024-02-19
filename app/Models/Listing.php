bec571f616e50c6f7c3f0e569b2e3f2f130fa438
Kristian <10221425@ucn.dk>
2024-02-19 10:33:52 +0100

    Soft Deletes (Putting Records Into Trash - To Be Restored)

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -4,12 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Listing extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $fillable = [
         'beds', 'baths', 'area', 'city', 'code', 'street', 'street_nr', 'price'
