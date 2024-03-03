d2e4d1b00d5225ff0734a97bf5e7c29b5652d8fa
Kristian <10221425@ucn.dk>
2024-03-03 14:45:29 +0100

    Seeing Which Offer is Sold

--- resources/js/Pages/MyAccount/Show.vue
+++ resources/js/Pages/MyAccount/Show.vue
@@ -1,8 +1,6 @@
 <template>
   <div class="mb-4">
-    <Link 
-      :href="route('my-account.listing.index')"
-    >
+    <Link :href="route('my-account.listing.index')">
       ← Go back to Listings
     </Link>
   </div>
@@ -10,41 +8,39 @@
   <section class="flex flex-col-reverse md:grid md:grid-cols-12 gap-4">
     <!-- Show this if there are no offers -->
     <Box v-if="!hasOffers" class="flex md:col-span-7 items-center">
-      <div class="w-full text-center font-medium text-gray-500">
-        No offers
-      </div>
+      <div class="w-full text-center font-medium text-gray-500">No offers</div>
     </Box>
     <!-- Show this if there are offers -->
-    <div v-else class="md:col-span-7 items-center">
+    <div v-else class="md:col-span-7 flex flex-col gap-4">
       <Offer
-        v-for="offer in listing.offers" 
-        :key="offer.id" class="mb-4"
+        v-for="offer in listing.offers"
+        :key="offer.id"
         :offer="offer"
         :listing-price="listing.price"
       />
     </div>
 
-    <Box class="md:col-span-5">
-      <template #header>Basic Info</template>
-      <Price :price="listing.price" class="text-2xl font-bold" />
+    <div class="md:col-span-5">
+      <Box>
+        <template #header>Basic Info</template>
+        <Price :price="listing.price" class="text-2xl font-bold" />
 
-      <ListingSpace :listing="listing" class="text-lg" />
-      <ListingAddress :listing="listing" class="text-gray-500" />
-    </Box>
+        <ListingSpace :listing="listing" class="text-lg" />
+        <ListingAddress :listing="listing" class="text-gray-500" />
+      </Box>
+    </div>
   </section>
 </template>
 
 <script setup>
-import ListingAddress from '@/Components/ListingAddress.vue'
-import ListingSpace from '@/Components/ListingSpace.vue'
-import Price from '@/Components/Price.vue'
-import Box from '@/Components/UI/Box.vue'
-import { computed } from 'vue'
-import Offer from '@/Pages/MyAccount/Show/Components/Offer.vue'
+import ListingAddress from "@/Components/ListingAddress.vue";
+import ListingSpace from "@/Components/ListingSpace.vue";
+import Price from "@/Components/Price.vue";
+import Box from "@/Components/UI/Box.vue";
+import { computed } from "vue";
+import Offer from "@/Pages/MyAccount/Show/Components/Offer.vue";
 
-const props = defineProps({ listing: Object })
+const props = defineProps({ listing: Object });
 
-const hasOffers = computed(
-  () => props.listing.offers.length,
-)
+const hasOffers = computed(() => props.listing.offers.length);
 </script>
--- resources/js/Pages/MyAccount/Show/Components/Offer.vue
+++ resources/js/Pages/MyAccount/Show/Components/Offer.vue
@@ -1,6 +1,13 @@
 <template>
   <Box>
-    <template #header>Offer #{{ offer.id }}</template>
+    <template #header
+      >Offer #{{ offer.id }}
+      <span
+        v-if="offer.accepted_at"
+        class="dark:bg-green-900 dark:text-green-200 bg-green-200 text-green-900 p-1 rounded-md uppercase ml-1"
+        >accepeted</span
+      ></template
+    >
 
     <section class="flex items-center justify-between">
       <div>
@@ -10,20 +17,18 @@
           Difference <Price :price="difference" />
         </div>
 
-        <div class="text-gray-500 text-sm">
-          Made by {{ offer.bidder.name }}
-        </div>
+        <div class="text-gray-500 text-sm">Made by {{ offer.bidder.name }}</div>
 
-        <div class="text-gray-500 text-sm">
-          Made on {{ madeOn }}
-        </div>
+        <div class="text-gray-500 text-sm">Made on {{ madeOn }}</div>
       </div>
       <div>
         <!-- :href="route('my-account.offer.accept', { offer: offer.id })" -->
         <Link
+          v-if="notSold"
           :href="route('my-account.offer.accept', offer.id)"
-          class="btn-outline text-xs font-medium" 
-          as="button" method="put"
+          class="btn-outline text-xs font-medium"
+          as="button"
+          method="put"
         >
           Accept
         </Link>
@@ -33,18 +38,18 @@
 </template>
 
 <script setup>
-import Price from '@/Components/Price.vue'
-import Box from '@/Components/UI/Box.vue'
-import { computed } from 'vue'
+import Price from "@/Components/Price.vue";
+import Box from "@/Components/UI/Box.vue";
+import { computed } from "vue";
 
 const props = defineProps({
   offer: Object,
   listingPrice: Number,
-})
-const difference = computed(
-  () => props.offer.amount - props.listingPrice,
-)
-const madeOn = computed(
-  () => new Date(props.offer.created_at).toDateString(),
-)
+});
+const difference = computed(() => props.offer.amount - props.listingPrice);
+const madeOn = computed(() => new Date(props.offer.created_at).toDateString());
+// Not sold if not accepted and not rejected
+const notSold = computed(
+  () => !props.offer.accepted_at && !props.offer.rejected_at
+);
 </script>
